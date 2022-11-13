6d8041b97a03df0f3032f09d3ebe8e0b83c87e0b
dariaveremey <daria.veremey.v@gmail.com>
2022-11-13 20:53:32 +0300

    Add ProximityCamera

---
+++ Assets/Scripts/Game/CameraServices/CameraSwitcher.cs
@@ -0,0 +1,42 @@
+﻿using Cinemachine;
+using UnityEngine;
+
+namespace Split.Game.CameraServices
+{
+    public class CameraSwitcher : MonoBehaviour
+
+    {
+        [SerializeField] private CinemachineVirtualCamera _minCamera;
+        [SerializeField] private CinemachineVirtualCamera _maxCamera;
+
+        private bool _overWorldCamera = true;
+
+        private void Update()
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+            {
+                SwitchPriority();
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+            {
+                SwitchPriority();
+            }
+        }
+
+        private void SwitchPriority()
+        {
+            if (_overWorldCamera)
+            {
+                _maxCamera.Priority = 10;
+                _minCamera.Priority = 0;
+            }
+            else
+            {
+                _maxCamera.Priority = 0;
+                _minCamera.Priority = 10;
+            }
+
+            _overWorldCamera = !_overWorldCamera;
+        }
+    }
+}
